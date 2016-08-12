e5feffce18e14e1eb1a119e53129403e7bfbae4f
weiucsd <w5wei@eng.ucsd.edu>
2016-08-12 14:40:43 -0700

    utf -8 test2

--- tutorial/tutorial/spiders/douyu_spider.py
+++ tutorial/tutorial/spiders/douyu_spider.py
@@ -11,12 +11,12 @@ class DouyuSpider(scrapy.Spider):
     def parse(self, response):
         for sel in response.xpath('//div[@id = "live-list-content"]/ul/li'):
             item = ZhiboItem()
-            item['title'] = sel.xpath('a/@title').extract()[0].encode('utf-8')
+            item['title'] = sel.xpath('a/@title').extract()[0]
             item['link'] = "http://www.douyutv.com" + sel.xpath('a/@href').extract()[0]
             num =  sel.xpath('a//span[@class="dy-num fr"]/text()').extract()[0]
             item['view'] = num
             item['img_url'] = sel.xpath('a//span[@class = "imgbox"]/img/@data-original').extract()[0]
-            item['zhubo'] = sel.xpath('a//span[@class = "dy-name ellipsis fl"]/text()').extract()[0].encode('utf-8')
+            item['zhubo'] = sel.xpath('a//span[@class = "dy-name ellipsis fl"]/text()').extract()[0]
             item['web'] = "douyu"
             item['cate'] = "ls"
             # item['active'] = True
