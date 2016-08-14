7f6d89ffe7a6629a297c59d77654c5656b1ef183
weiucsd <w5wei@eng.ucsd.edu>
2016-08-14 03:17:59 -0700

    test gallary 2

--- gallary.php
+++ gallary.php
@@ -7,163 +7,29 @@
 	</head>
 	<body>
 
-		<h2>Responsive Image Gallery</h2>
-		<h4>Resize the browser window to see the effect.</h4>
+		<h2>HearthStone</h2>
 
+		<?php
+			include 'db.php';
+			$conn = dbConnect();
+
+			$sql = "SELECT * FROM HearthStones WHERE view > 100 ORDER BY view DESC;";
+			$result = $conn->query($sql);
+			while($row = $result->fetch_assoc()) {			
+		?> 		
+		
 		<div class="responsive">
 		  <div class="img">
-		    <a target="_blank" href="img_fjords.jpg">
-		      <img src="http://www.w3schools.com/css/img_fjords.jpg" alt="Trolltunga Norway" width="300" height="200">
+		    <a target="_blank" href=<?= $row['link']?>>
+		      <img src=<?= $row['img_url']?>>
 		    </a>
-		    <div class="desc">Add a description of the image here</div>
+		    <div class="desc"><?= $row['title']?></div>
 		  </div>
 		</div>
 
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_forest.jpg">
-		      <img src="http://www.w3schools.com/css/img_forest.jpg" alt="Forest" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_lights.jpg">
-		      <img src="http://www.w3schools.com/css/img_lights.jpg" alt="Northern Lights" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_mountains.jpg">
-		      <img src="http://www.w3schools.com/css/img_mountains.jpg" alt="Mountains" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_fjords.jpg">
-		      <img src="http://www.w3schools.com/css/img_fjords.jpg" alt="Trolltunga Norway" width="300" height="200">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_forest.jpg">
-		      <img src="http://www.w3schools.com/css/img_forest.jpg" alt="Forest" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_lights.jpg">
-		      <img src="http://www.w3schools.com/css/img_lights.jpg" alt="Northern Lights" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_mountains.jpg">
-		      <img src="http://www.w3schools.com/css/img_mountains.jpg" alt="Mountains" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_fjords.jpg">
-		      <img src="http://www.w3schools.com/css/img_fjords.jpg" alt="Trolltunga Norway" width="300" height="200">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_forest.jpg">
-		      <img src="http://www.w3schools.com/css/img_forest.jpg" alt="Forest" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_lights.jpg">
-		      <img src="http://www.w3schools.com/css/img_lights.jpg" alt="Northern Lights" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_mountains.jpg">
-		      <img src="http://www.w3schools.com/css/img_mountains.jpg" alt="Mountains" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_fjords.jpg">
-		      <img src="http://www.w3schools.com/css/img_fjords.jpg" alt="Trolltunga Norway" width="300" height="200">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_forest.jpg">
-		      <img src="http://www.w3schools.com/css/img_forest.jpg" alt="Forest" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_lights.jpg">
-		      <img src="http://www.w3schools.com/css/img_lights.jpg" alt="Northern Lights" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="responsive">
-		  <div class="img">
-		    <a target="_blank" href="img_mountains.jpg">
-		      <img src="http://www.w3schools.com/css/img_mountains.jpg" alt="Mountains" width="600" height="400">
-		    </a>
-		    <div class="desc">Add a description of the image here</div>
-		  </div>
-		</div>
-
-		<div class="clearfix"></div>
-
-		<div style="padding:6px;">
-		  <p>This example use media queries to re-arrange the images on different screen sizes: for screens larger than 700px wide, it will show four images side by side, for screens smaller than 700px, it will show two images side by side. For screens smaller than 500px, the images will stack vertically (100%).</p>
-		  <p>You will learn more about media queries and responsive web design later in our CSS Tutorial.</p>
-		</div>
+		<?php
+			}//while end
+		?>
 
 	</body>
 </html>
