843d4df52b80db86f877b7bcae6140d7556b504a
weiucsd <w5wei@eng.ucsd.edu>
2016-08-15 16:21:31 -0700

    google translate

--- gallary.php
+++ gallary.php
@@ -9,6 +9,13 @@
 
 		<h2>HearthStone</h2>
 
+		<!-- google translate -->
+		<div id="google_translate_element"></div><script type="text/javascript">
+		function googleTranslateElementInit() {
+		  new google.translate.TranslateElement({pageLanguage: 'zh-CN', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
+		}
+		</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        
 		<?php
 			include 'db.php';
 			$conn = dbConnect();
