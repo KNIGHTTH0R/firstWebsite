683d24de76462897c2b803944bd70355c115a52f
weiucsd <w5wei@eng.ucsd.edu>
2016-08-25 14:44:50 -0700

    sidebar color 3

--- gallary.php
+++ gallary.php
@@ -53,7 +53,7 @@
 		        <li><a href="?cate=hs"
 		        	<?php
 		        		if ($cate == "hs") {
-		        			echo " style='background-color:#555;'";
+		        			echo " style='color:#eee; background-color:#2a5caa;'";
 		        		}
 		        	?>
 		        	>炉石传说</a></li>
