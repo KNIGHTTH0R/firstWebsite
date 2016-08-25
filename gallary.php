390a70a5225890251be97aa21a09e949ea8d7c81
weiucsd <w5wei@eng.ucsd.edu>
2016-08-25 12:03:39 -0700

    sidebar reset

--- gallary.php
+++ gallary.php
@@ -50,13 +50,7 @@
 		<!-- Left sidebar -->
  		<div class="sidebar">
 	    	<ul>
-		        <li><a href="?cate=hs"
-					<?php
-						if (cate == "hs") {
-							echo 'color:#eee; background-color:#2a5caa;';
-						}
-					?>
-		        	>炉石传说</a></li>
+		        <li><a href="?cate=hs">炉石传说</a></li>
 		        <li><a href="?cate=lol">LOL</a></li>
 		        <li><a href="?cate=dota">Dota</a></li>
 		        <li><a href="?cate=ow">守望先锋</a></li>
