241545a859e1dce6326adefd44b05df05ec7afa0
weiucsd <w5wei@eng.ucsd.edu>
2016-08-31 12:18:10 -0700

    php add girl category

--- gallary.php
+++ gallary.php
@@ -18,8 +18,8 @@
 			if ($cate == "") {
 				$cate = "hs";
 			}
-			$cates=array("hs","lol","dota","ow","sc","dnf");
-			$names=array("炉石传说","LOL","Dota","守望先锋","星际争霸","DNF");
+			$cates=array("hs","lol","dota","ow","sc","dnf","girl");
+			$names=array("炉石传说","LOL","Dota","守望先锋","星际争霸","DNF","帅哥美女");
 			$webs=array("douyu"=>"斗鱼","huya"=>"虎牙","panda"=>"熊猫","zhanqi"=>"战旗","quanmin"=>"全民","huomao"=>"火猫","longzhu"=>"龙珠");
 		?>		
 		<!---start-header-->
